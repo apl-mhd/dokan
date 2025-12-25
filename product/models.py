dc3b2d4ce2bb19afa607d3d4d0e4271f48a5b4bd
dxh-apel <git-apel@devxhub.com>
2025-12-25 20:58:45 +0600

    project intialize

--- product/models.py
+++ product/models.py
@@ -3,10 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+class UnitCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Unit(models.Model):
     name = models.CharField(max_length=128)
-    unit_value = models.IntegerField(default=1)
-    abbreviation = models.CharField(null=True, blank=True, max_length=10)
+    conversion_factor = models.DecimalField(max_digits=10, decimal_places=4, default=1)
+    is_base_unit = models.BooleanField(default=False)
+    category = models.ForeignKey(UnitCategory, on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
