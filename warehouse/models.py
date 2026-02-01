7facfd87fc9cdf4c75fd1bc7cde7379b4ec4c2cd
dxh-apel <git-apel@devxhub.com>
2026-02-01 18:59:36 +0600

    warehouse update

--- warehouse/models.py
+++ warehouse/models.py
@@ -5,7 +5,9 @@ from company.models import Company
 class Warehouse(models.Model):
     name = models.CharField(max_length=20)
     company = models.ForeignKey(Company, on_delete=models.PROTECT, related_name='warehouses')
-    location = models.CharField(max_length=50)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    is_default = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
